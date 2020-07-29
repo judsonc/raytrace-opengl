16d8699d86ebd688f98c37aaf27d38374618a838
Judson Costa <judson.scosta@hotmail.com>
2020-07-29 20:12:15 -0300

    mudança nas paredes

--- main.cpp
+++ main.cpp
@@ -12,10 +12,6 @@
 #define WIDTH 640
 #define HEIGHT 480
 
-#define STEP_LIGHT_R 0.1f //5 graus
-#define STEP_R 0.1f       //5 graus
-#define STEP_D 2.0f
-
 float angX = 0;
 float angY = 0;
 float angZ = 0;
@@ -33,25 +29,13 @@ void display(void)
 };
 
 //inicializa o universo
-/***** Informacoes para calculo de projecao ****/
-/*
-* Informacoes ficam em RayTracer.viewerData, essas informacoes possuem um default
-* caso nao seja setado apos a construcao de um objeto RayTracer, sera:
-* znear(default = 0.01), zfar (default = 10000.0)
-* Camera: from(0.0 ,0.0, -0.5), lookAt(0.0, 0.0, 0.0), vUp(0.0, 1.0, 0.0)
-* angulo de abertura do pespective (default = 60.0)
-* dimensoes da tela: 640x480;
-* posicionamento e orientacao da Camera;
-*/
-//Cria e posiciona os Objetos
-//Cria e posiciona os pontos de luz
 void init(int win_width, int win_height)
 {
   glClearColor(0.0, 0.0, 0.0, 0.0);
   glClear(GL_COLOR_BUFFER_BIT);
   Material material;
   Vec pos, normal;
-  float radius = 1.0;
+  float radius;
 
   //Configurando a matriz de projecao
   raytracer.viewer.setWindowSize(win_width, win_height); //atualiza dimensoes da tela
@@ -61,78 +45,52 @@ void init(int win_width, int win_height)
   raytracer.world.lightEnv = 1.0;
   raytracer.world.ka = 0.0;
 
-  // Moon
-  material.n_shiny = 10.0;
+  //Moon
   material.kr = 0.0;
+  material.n_shiny = 10.0;
   material.color = Vec(1.0, 1.0, 1.0);
   material.setKs(0.1, raytracer.world.ka);
   pos = Vec(0, 0, 10.0);
   radius = 0.5;
   raytracer.world.objs.push_back(new Sphere(material, pos, radius));
 
-  // Earth
-  material.n_shiny = 200.0;
+  //Earth
+  material.kr = 0.0;
+  material.n_shiny = 10.0;
   material.color = Vec(0.17, 0.46, 1.0);
-  material.setKs(0.5, raytracer.world.ka);
-  material.kr = 0.6;
-  pos = Vec(0, 0, 0);
+  material.setKs(0.1, raytracer.world.ka);
+  pos = Vec(0.0, 0.0, 0.0);
   radius = 1.83;
   raytracer.world.objs.push_back(new Sphere(material, pos, radius));
 
-  // Wall
-  material.kr = 0.1;
-  material.n_shiny = 30.0;
-  material.color = Vec(0.6, 0.6, 0.6);
-  material.setKs(0.7, raytracer.world.ka);
-  pos = Vec(0, 0, 90.0);
-  normal = Vec(0, 0, -1.0);
-  raytracer.world.objs.push_back(new Plane(material, pos, normal));
-
-  // Wall
+  //Wall normal axis Z
   material.kr = 0.0;
   material.n_shiny = 30.0;
-  material.color = Vec(0.6, 0.6, 0.6);
+  material.color = Vec(0.3, 0.3, 0.3);
   material.setKs(0.1, raytracer.world.ka);
-  pos = Vec(90, 0, 0.0);
-  normal = Vec(-1, 0, 0);
+  pos = Vec(0.0, 0.0, 90.0);
+  normal = Vec(0.0, 0.0, -1.0);
   raytracer.world.objs.push_back(new Plane(material, pos, normal));
 
-  // Wall
+  //Wall normal axis Y
   material.kr = 0.0;
   material.n_shiny = 30.0;
-  material.color = Vec(0.6, 0.6, 0.6);
-  material.setKs(0.1, raytracer.world.ka);
-  pos = Vec(-90, 0, 0.0);
-  normal = Vec(1, 0, 0);
-  raytracer.world.objs.push_back(new Plane(material, pos, normal));
-
-  // Wall
-  material.kr = 0.0;
-  material.n_shiny = 30.0;
-  material.color = Vec(0.6, 0.6, 0.6);
-  material.setKs(0.6, raytracer.world.ka);
-  pos = Vec(0, 0, -90.0);
-  normal = Vec(0, 0, 1);
-  raytracer.world.objs.push_back(new Plane(material, pos, normal));
-
-  // Wall
-  material.kr = 0.0;
-  material.n_shiny = 30.0;
-  material.color = Vec(0.6, 0.6, 0.6);
-  material.setKs(0.1, raytracer.world.ka);
-  pos = Vec(0, 90, 0.0);
-  normal = Vec(0, -1, 0);
-  raytracer.world.objs.push_back(new Plane(material, pos, normal));
-
-  // Wall
-  material.kr = 0.0;
-  material.n_shiny = 30.0;
-  material.color = Vec(0.6, 0.6, 0.6);
+  material.color = Vec(0.3, 0.3, 0.3);
   material.setKs(0.3, raytracer.world.ka);
-  pos = Vec(0, -90, 0.0);
-  normal = Vec(0, 1, 0);
+  pos = Vec(0.0, -90.0, 0.0);
+  normal = Vec(0.0, 1.0, 0.0);
   raytracer.world.objs.push_back(new Plane(material, pos, normal));
 
+  //Wall normal axis X
+  material.kr = 0.0;
+  material.n_shiny = 30.0;
+  material.color = Vec(0.3, 0.3, 0.3);
+  material.setKs(0.1, raytracer.world.ka);
+  pos = Vec(60.0, 0.0, 0.0);
+  normal = Vec(-1.0, 0.0, 0.0);
+  raytracer.world.objs.push_back(new Plane(material, pos, normal));
+
+  //Sunlight
   raytracer.world.lights.push_back(LightSource(Vec(1.5, 5.0, -5.0), Vec(1.0, 1.0, 1.0)));
 };
 
